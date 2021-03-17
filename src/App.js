63f31427743dfd91854b392c73ea0c16090852e6
hodamoustafa <h.m.n.moustafa@gmail.com>
2021-03-17 17:40:44 -0400

    added weekly view and fixed positions

--- src/App.js
+++ src/App.js
@@ -9,27 +9,61 @@ import './main.css'
 
 function App() {
   return (
-      <body>
         <div class="app-wrap">
           <main>
-            <section class="location">
+
+            {/* <div class="location">
               <div class="hello">Good Morning Hoda.</div>
               <div class="city">San Francisco, CA</div>
               <div class="date">Monday | Mar 15 | 10:15 AM</div>
-            </section>
+            </div> */}
+              <div class="hello"> Good Morning User! </div>
+
+              <div class="city">San Francisco, CA</div>
             <div class="current">
+              
+              {/* <br></br> */}
+              
+              <div class="date">Monday | Mar 15 | 10:15 AM</div>
               <div class="temp">15<span>°F</span></div>
+              
               <div class="weather">Cloudy</div>
               <div class="hi-low">44°F / 33°F</div>
             </div>
+
             <section class="weekly">
-              <div class="mon">15<span>°F</span></div>
+              {/* <div class="mon">15<span>°F</span></div>
               <div class="mon_weather">Cloudy</div>
-              <div class="mon_hi-low">44°F / 33°F</div>
-            </section>
+              <div class="mon_hi-low">44°F / 33°F</div> */}
+              <div class="week_view">This Week</div>
+            
+                <div class="divTable weeklyView">
+                <div class="divTableHeading">
+                <div class="divTableRow">
+                <div class="divTableHead">Tues</div>
+                <div class="divTableHead">Wed</div>
+                <div class="divTableHead">Thur</div>
+                <div class="divTableHead">Fri</div>
+                <div class="divTableHead">Sat</div>
+                <div class="divTableHead">Sun</div>
+                </div>
+                </div>
+                <div class="divTableBody">
+                <div class="divTableRow">
+                <div class="divTableCell">55°/49°</div>
+                <div class="divTableCell">56°/49°</div>
+                <div class="divTableCell">57°/49°</div>
+                <div class="divTableCell">57°/46°</div>
+                <div class="divTableCell">58°/44°</div>
+                <div class="divTableCell">60°/47°</div>
+                </div>
+                </div>
+                </div>
+
+          </section>
+
           </main>
         </div>
-      </body>
   );
 }
 
