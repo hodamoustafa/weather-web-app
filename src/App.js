9cc48f270e8817cce8de976692939960c4ceeaa3
hodamoustafa <h.m.n.moustafa@gmail.com>
2021-03-17 02:56:58 -0400

    initial look

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import styles from "./Desktop1.scss";
+import Desktop1 from './Desktop1';
+
+//var BackgroundBase = require("./BackgroundBase.png")
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Desktop1/> 
     </div>
-  );
+  )
 }
 
 export default App;
